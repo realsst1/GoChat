bac83649d972543e3f54336683791556d84243d9
realsst1 <shreyesh2008@gmail.com>
2018-06-20 19:09:21 +0530

    Login added

--- app/src/main/java/com/example/shreyesh/gochat/LoginActivity.java
+++ app/src/main/java/com/example/shreyesh/gochat/LoginActivity.java
@@ -1,16 +1,31 @@
 package com.example.shreyesh.gochat;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Toolbar loginPageToolbar;
     private TextInputLayout loginEmail, loginPassword;
     private Button loginButtonLoginPage;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,46 @@ public class LoginActivity extends AppCompatActivity {
         setSupportActionBar(loginPageToolbar);
         getSupportActionBar().setTitle("Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        loginButtonLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getEditText().getText().toString();
+                String password = loginPassword.getEditText().getText().toString();
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Please fill all fields", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(LoginActivity.this, "Invalid Email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                progressDialog.setTitle("Logging In");
+                progressDialog.setMessage("Please wait while we check your credentials...");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+                login(email, password);
+            }
+        });
+
+
+    }
+
+    private void login(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    progressDialog.hide();
+                    Toast.makeText(LoginActivity.this, "Error " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void setupUIViews() {
