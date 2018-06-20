0174355b567d9985d61aaa1bd9548facdb44f3f2
realsst1 <shreyesh2008@gmail.com>
2018-06-20 15:03:58 +0530

    UI design started for Start and Reg (improvements,bug fixes)

--- app/src/main/java/com/example/shreyesh/gochat/RegisterActivity.java
+++ app/src/main/java/com/example/shreyesh/gochat/RegisterActivity.java
@@ -1,11 +1,15 @@
 package com.example.shreyesh.gochat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,6 +26,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Toolbar regPageToolbar;
     private FirebaseAuth firebaseAuth;
 
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +40,8 @@ public class RegisterActivity extends AppCompatActivity {
         setSupportActionBar(regPageToolbar);
         getSupportActionBar().setTitle("Create Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        progressDialog = new ProgressDialog(this);
+
 
         regCreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,9 +50,21 @@ public class RegisterActivity extends AppCompatActivity {
                 String name = regDisplayName.getEditText().getText().toString();
                 String password = regPassword.getEditText().getText().toString();
 
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(name) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "Please fill all fields", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(RegisterActivity.this, "Invalid email", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (password.length() < 8) {
+                    Toast.makeText(RegisterActivity.this, "Password length should atleast be 8", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                progressDialog.setTitle("Registering User");
+                progressDialog.setMessage("Please wait while we register you..");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
                 registerUser(name, email, password);
-
-
             }
         });
     }
@@ -55,10 +75,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
+                    progressDialog.dismiss();
                     Toast.makeText(RegisterActivity.this, "Registration Succesful", Toast.LENGTH_LONG).show();
                     startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                     finish();
                 } else {
+                    progressDialog.hide();
                     Toast.makeText(RegisterActivity.this, "Error " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
