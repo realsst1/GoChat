b7ee4fd9d92efd96871919d4feffd9a4f884f991
realsst1 <shreyesh2008@gmail.com>
2018-06-22 13:42:43 +0530

    Settings Page Completed

--- app/src/main/java/com/example/shreyesh/gochat/SettingsActivity.java
+++ app/src/main/java/com/example/shreyesh/gochat/SettingsActivity.java
@@ -1,12 +1,18 @@
 package com.example.shreyesh.gochat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -14,6 +20,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -25,6 +37,9 @@ public class SettingsActivity extends AppCompatActivity {
     private Button changeImage, changeStatus;
     private TextView displayName, userStatus;
     private FirebaseUser currentUser;
+    private StorageReference storageReference;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +49,8 @@ public class SettingsActivity extends AppCompatActivity {
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String userID = currentUser.getUid();
         databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(userID);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        progressDialog = new ProgressDialog(this);
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -45,6 +62,7 @@ public class SettingsActivity extends AppCompatActivity {
 
                 displayName.setText(name);
                 userStatus.setText(status);
+                Picasso.get().load(displayImage).into(circleImageView);
             }
 
             @Override
@@ -62,6 +80,21 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1, 1)
+                        .setMinCropResultSize(512, 512)
+                        .start(SettingsActivity.this);
+
+            }
+        });
+
+
+
     }
 
 
@@ -72,4 +105,52 @@ public class SettingsActivity extends AppCompatActivity {
         displayName = (TextView) findViewById(R.id.settingsDisplayName);
         userStatus = (TextView) findViewById(R.id.settingsStatusTextView);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                progressDialog.setTitle("Uploading Image");
+                progressDialog.setMessage("Please wait while we upload the image...");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+                Uri resultUri = result.getUri();
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                String userID = currentUser.getUid();
+
+                StorageReference filepath = storageReference.child("profile_pictures").child(userID + ".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            String downloadURI = task.getResult().getDownloadUrl().toString();
+                            databaseReference.child("image").setValue(downloadURI).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        progressDialog.dismiss();
+                                    } else {
+                                        progressDialog.hide();
+                                        Toast.makeText(SettingsActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            progressDialog.hide();
+                            Toast.makeText(SettingsActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
 }
