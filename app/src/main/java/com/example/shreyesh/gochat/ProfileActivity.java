72f95027616d30e4e6ff911c79496b0a4f9959ee
realsst1 <shreyesh2008@gmail.com>
2018-06-23 15:05:18 +0530

    Friend Request feature work started

--- app/src/main/java/com/example/shreyesh/gochat/ProfileActivity.java
+++ app/src/main/java/com/example/shreyesh/gochat/ProfileActivity.java
@@ -1,12 +1,19 @@
 package com.example.shreyesh.gochat;
 
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,9 +25,11 @@ public class ProfileActivity extends AppCompatActivity {
 
     private TextView profileDisplayName, profileStatus, profileFriendCount;
     private ImageView profileImage;
-    private Button sendRequestButton;
-    private DatabaseReference databaseReference;
+    private Button sendRequestButton, declineRequestButton;
+    private DatabaseReference databaseReference, friendRequestDatabase;
     private ProgressDialog progressDialog;
+    private String currentState;
+    private FirebaseUser currentUser;
 
 
     @Override
@@ -28,19 +37,28 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        //Initialize values
         profileDisplayName = (TextView) findViewById(R.id.profileDisplayName);
         profileStatus = (TextView) findViewById(R.id.profileStatus);
         profileFriendCount = (TextView) findViewById(R.id.friendsCountTextView);
         profileImage = (ImageView) findViewById(R.id.profileImage);
         sendRequestButton = (Button) findViewById(R.id.friendRequestButton);
+        declineRequestButton = (Button) findViewById(R.id.declineFriendRequest);
+
+        //ProgressBar
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Loading User Details");
         progressDialog.setMessage("Please wait while we load details...");
         progressDialog.setCanceledOnTouchOutside(false);
         progressDialog.show();
 
+        currentState = "notFriends";
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(getIntent().getStringExtra("user_id"));
+        final String userID = getIntent().getStringExtra("user_id");
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        friendRequestDatabase = FirebaseDatabase.getInstance().getReference().child("Friends");
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(userID);
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -63,5 +81,38 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+
+        //Friend Request
+        sendRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentState.equals("notFriends")) {
+                    friendRequestDatabase.child(currentUser.getUid()).child(userID).child("requestType").setValue("sent").addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                friendRequestDatabase.child(userID).child(currentUser.getUid()).child("requestType").setValue("received").addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(ProfileActivity.this, "Request Sent", Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(ProfileActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+
+
+
+
     }
 }
